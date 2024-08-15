a653f2eca9420005d2bde66df8ec81db61a12c6d
gitNP26 <nickeepowell26@gmail.com>
2024-08-15 19:28:37 +0100

    Added notation to explain function

--- PY4E-4.6.py
+++ PY4E-4.6.py
@@ -1,5 +1,15 @@
 
 def computepay(hours, rate):
+    """
+    This function calculates pay by multiplying hours by rate
+    For every hour over 40, rate is multiplied by 1.5.
+    Parameters:
+    hour (int): number of hours worked
+    rate (int): the hourly rate of pay
+
+    Returns:
+    pay(int): This is the total pay for rate by hours worked
+    """
     try:
         if hours <= 40:
             pay = hours*rate
